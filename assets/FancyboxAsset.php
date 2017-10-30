67d37d3e7a0e538a2be3b9355189ba5fcdbac700
Alexander Popov <b059ae@gmail.com>
2017-10-30 23:23:57 +0300

    Добавлены thumb для fancybox

--- assets/FancyboxAsset.php
+++ assets/FancyboxAsset.php
@@ -7,9 +7,11 @@ class FancyboxAsset extends \yii\web\AssetBundle
 
     public $css = [
         'jquery.fancybox.css',
+        'helpers/jquery.fancybox-thumbs.css'
     ];
     public $js = [
-        'jquery.fancybox.pack.js'
+        'jquery.fancybox.pack.js',
+        'helpers/jquery.fancybox-thumbs.js'
     ];
 
     public $depends = ['yii\web\JqueryAsset'];
